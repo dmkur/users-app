a73519764cfcc8787d9c5b0673e98efa86987c48
“dmkur” <fgameua@gmail.com>
2023-06-12 21:02:13 +0300

    add docker files

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-require('dotenv').config();
+// require('dotenv').config();
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
 const cors = require('cors');
